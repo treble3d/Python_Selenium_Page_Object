5fabd495c5b1a952a3463e64d9dbc8b213038aea
treble3d <sviathut@gmail.com>
2022-09-27 14:08:47 +0300

    add enumeration of links marked as bad

--- conftest.py
+++ conftest.py
@@ -1,7 +1,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en',
@@ -27,5 +26,4 @@ def browser(request):
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
-    # time.sleep(400)
     browser.quit()
