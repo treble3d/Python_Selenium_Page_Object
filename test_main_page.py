e5a374f43a515d8c92f67634c951f79f6e4b1036
treble3d <sviathut@gmail.com>
2022-09-28 16:02:02 +0300

    add class test_login link

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from .pages.basket_page import BasketPage
+from .pages.main_page import MainPage
+import pytest
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
@@ -7,3 +9,17 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket()
     page.should_basket_empty()
     page.should_message_basket_empty_present()
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/"
+        page = MainPage(browser,link)
+        page.open()
+        page.go_to_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
