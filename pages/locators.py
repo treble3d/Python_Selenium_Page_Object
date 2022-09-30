a18a3d64d3ca964fefaeeff1bc64403890243466
treble3d <sviathut@gmail.com>
2022-09-30 14:21:36 +0300

    add reg locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.common.by import By
 
-
 class ProductPageLocators():
     BASKET_BUTTON_ADD = (By.CSS_SELECTOR, "button.btn-add-to-basket")
     BASKET_PRODUCT_NAME = (By.CSS_SELECTOR, ".alertinner strong")
@@ -13,9 +12,16 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, "span>a.btn")
     BASKET_FULL = (By.CSS_SELECTOR, "form.basket_summary")
     BASKET_EMPTY = (By.CSS_SELECTOR, "div.content p")
+
+class LoginPageLocators():
+    REG_EMAIL = (By.CSS_SELECTOR, "input[name=registration-email]")
+    REG_PASSWORD = (By.CSS_SELECTOR, "input[name=registration-password1]")
+    REG_PASSWORD_REP = (By.CSS_SELECTOR, "input[name=registration-password2]")
+    REG_SUBMIT_BUTTON = (By.CSS_SELECTOR, "button[name=registration_submit]")
+    REG_SUCCESS = (By.CSS_SELECTOR, "div.alert-success")
