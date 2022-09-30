fd15376a088422062c283491ffeb701310934921
treble3d <sviathut@gmail.com>
2022-09-30 14:24:20 +0300

    add login_page add register new user

---
+++ pages/login_page.py
@@ -0,0 +1,15 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        input1 = self.browser.find_element(*LoginPageLocators.REG_EMAIL)
+        input1.send_keys(email)
+        input2 = self.browser.find_element(*LoginPageLocators.REG_PASSWORD)
+        input2.send_keys(password)
+        input3 = self.browser.find_element(*LoginPageLocators.REG_PASSWORD_REP)
+        input3.send_keys(password)
+        button = self.browser.find_element(*LoginPageLocators.REG_SUBMIT_BUTTON)
+        button.click()
+        assert self.is_element_present(*LoginPageLocators.REG_SUCCESS), "Error registration"
+
